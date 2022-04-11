ced059db038ed5bb7d6501e8d8ba970d3cf3f26d
Kittipos <ki11ip0.s.a.s@gmail.com>
2022-04-11 16:10:13 +0700

    add `read_HRV_reports()` update `parse_HRV_reports()` description

--- R/HRV_reports.R
+++ R/HRV_reports.R
@@ -1,12 +1,78 @@
-#' Parse HRV Report file
+
+
+# Read HRV Report ---------------------------------------------------------
+
+
+#' Read LabChart's HRV Report File
 #'
-#' Parse HRV Report file from a character vector to a tibble.
+#' @description
+#' Read and transform LabChart's HRV report file (`.txt`) to a tidy [tibble](https://tibble.tidyverse.org)
+#' with one row per file.
+#'
+#' @details [read_HRV_reports()] reads contents of LabChart's HRV report from text file (`.txt`)
+#' into character vector using [readtext::readtext()] function with default text encoding as "UTF-16LE".
+#' Then, each key-value fields of the character vector will be extracted and transform into a [tibble](https://tibble.tidyverse.org)
+#' by regular expression pattern matching as in [parse_HRV_reports()].
+#'
+#' The result will be a tibble with column names corresponding to "key" in the input file(s),
+#' and values in each rows mapped to "value" from the input file(s).
+#'
+#' @param file (Character) Path to LabChart's HRV report (`.txt`) files to be read.
+#' Path can point to an individual text file (`.txt`) or a folder which contains those files.
+#' @param format_cols (Logical) If `TRUE` formats appropriate output variables as factor, integer, or double, and compute `NN50_percent` (pNN50) from `NN50_count/Normals_count`
+#' @param encoding (Character) Text encoding of the HRV report files.
+#' Default value is set to "UTF-16LE" as it works well with my testing data.
+#' If the result seems abnormal, try different text encoding (visit [readtext manual](https://readtext.quanteda.io/index.html) to see all available options)
+#' @param ... extra arguments passed to [readtext::readtext()]
+#'
+#' @return A tibble of HRV report (one row per subject)
+#' @export
+#'
+#' @examples
+#' # Path to `HRV/` folder containing example HRV report files (`.txt`)
+#' path <- sipsANS_example("HRV")
+#' # Read into tibble
+#' read_HRV_reports(path)
+read_HRV_reports <- function(file,
+                             format_cols = TRUE,
+                             encoding = "UTF-16LE",
+                             ...
+){
+  # Read Raw
+  df_raw <- readtext::readtext(file = file,
+                               encoding = encoding,
+                               ...
+  )
+  # Parse Character to HRV tibble
+  df_hrv <- parse_HRV_reports(df_raw[["text"]],
+                              format_cols = format_cols)
+  # Bind `doc_id` to HRV tibble
+  df_out <- dplyr::bind_cols(doc_id = df_raw[["doc_id"]], df_hrv)
+  df_out
+}
+
+
+# Parse HRV Report --------------------------------------------------------
+
+
+
+#' Parse LabChart's HRV Report
+#'
+#' @description
+#' Parse and transform LabChart's HRV report from a character vector to a tidy [tibble](https://tibble.tidyverse.org)
+#'
+#' @details
+#' [parse_HRV_reports()] extracts key and value pairs from each fields of LabChart's HRV report supplied as character vectors
+#' using regular expression for pattern matching. Extracted data then transform into a [tibble](https://tibble.tidyverse.org)
+#' with "key" mapping to column names and "value" mapping to corresponding cells.
+#' If `x` is given as character vector of length > 1, there will be multiple rows of tibble with each rows corresponding to each input character vectors.
 #'
 #' @param x (Character) Character vectors of HRV report, each elements
 #' corresponding to one report.
-#' @param format_cols If `TRUE` format appropriate output variables as factor, integer, or double. Compute `NN50_percent` (pNN50) from `NN50_count/Normals_count`
+#' @param format_cols (Logical) If `TRUE` formats appropriate output variables as factor, integer, or double, and compute `NN50_percent` (pNN50) from `NN50_count/Normals_count`
 #'
-#' @return A tibble
+#' @seealso [read_HRV_reports()]: read LabChart's HRV report from file(s) path.
+#' @return A tibble of HRV report (one row per subject)
 #' @export
 #' @include HRV_field.R
 #'
--- tests/testthat/test-HRV_reports.R
+++ tests/testthat/test-HRV_reports.R
@@ -1,20 +1,40 @@
 
 
+hrv_tbl_names <- c("doc_id", "File_LabChart", "Channel", "Date", "Start_time", "End_time",
+                   "Name", "Gender", "Age", "Beats_tot", "Rec_length", "Class_bound",
+                   "Discontinuities", "Beats_inserted", "Beats_deleted",
+                   "NN_max", "NN_min", "NN_range", "NN_mean", "NN_median",
+                   "HR_avg", "SDNN", "SD_del_NN", "RMSSD", "Normals_count",
+                   "Ectopics_count", "Artifacts_count", "NN50_count", "NN50_percent",
+                   "Spec_intv", "Spec_mean_NN", "Power_tot", "VLF_freq", "VLF",
+                   "LF_freq", "LF", "LF_nu", "HF_freq", "HF", "HF_nu", "LF_HF")
+
+
+# Parse HRV -------------------------------------------------------------------
+
+
+
 test_that("parse_HRV_reports() works", {
 
   hrv_tbl <- parse_HRV_reports(hrv_chr_raw)
   # Tibble Class
   expect_s3_class(hrv_tbl, "tbl_df")
-  # Check Col Nmaes
-  expect_named(hrv_tbl,
-               c("File_LabChart", "Channel", "Date", "Start_time", "End_time",
-                 "Name", "Gender", "Age", "Beats_tot", "Rec_length", "Class_bound",
-                 "Discontinuities", "Beats_inserted", "Beats_deleted",
-                 "NN_max", "NN_min", "NN_range", "NN_mean", "NN_median",
-                 "HR_avg", "SDNN", "SD_del_NN", "RMSSD", "Normals_count",
-                 "Ectopics_count", "Artifacts_count", "NN50_count", "NN50_percent",
-                 "Spec_intv", "Spec_mean_NN", "Power_tot", "VLF_freq", "VLF",
-                 "LF_freq", "LF", "LF_nu", "HF_freq", "HF", "HF_nu", "LF_HF")
-)
+  # Check Col Names
+  expect_named(hrv_tbl, hrv_tbl_names[-1]) # Excluding `doc_id`
+
+})
+
+
+
+# Read HRV ----------------------------------------------------------------
+
+
+test_that("read_HRV_reports() works", {
+
+  hrv_tbl <- read_HRV_reports(path_hrv)
+  # Tibble Class
+  expect_s3_class(hrv_tbl, "tbl_df")
+  # Check Col Names
+  expect_named(hrv_tbl, hrv_tbl_names)
 
 })
